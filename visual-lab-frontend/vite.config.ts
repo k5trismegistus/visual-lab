0be686b1158fa30b03779cfb1714763b21d523ce
keigo yamamoto <k5.trismegistus@gmail.com>
2024-11-21 00:33:58 +0900

    :wrench: Use https in development

--- visual-lab-frontend/vite.config.ts
+++ visual-lab-frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -6,9 +7,27 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
+  const server = (() => {
+    switch (process.env.NODE_ENV) {
+      case "production":
+        return { host: "0.0.0.0" };
+      case "development":
+      default:
+        return {
+          host: "0.0.0.0",
+          https: {
+            key: fs.readFileSync("./lvh.me-key.pem"),
+            cert: fs.readFileSync("./lvh.me.pem"),
+          },
+        };
+    }
+  })();
+
   return {
+    server,
     define: {
-      "process.env.VITE_API_ENDPOINT": JSON.stringify(env.VITE_API_ENDPOINT),
+      "process.env.VITE_API_BASE": JSON.stringify(env.VITE_API_BASE),
+      "process.env.VITE_API_KEY": JSON.stringify(env.VITE_API_KEY),
     },
     plugins: [TanStackRouterVite({}), react()],
   };
